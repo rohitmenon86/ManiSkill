2bcdf0914e3c8b4b5ae4f008c35d303d4af1e159
Stone Tao <stonezt2019@gmail.com>
2024-04-05 14:10:38 -0700

    Create mem_leak_test.py

---
+++ manualtest/mem_leak_test.py
@@ -0,0 +1,27 @@
+import gc
+
+import gymnasium as gym
+import psutil
+
+import mani_skill.envs
+from mani_skill.vector.wrappers.gymnasium import ManiSkillVectorEnv
+
+
+def main():
+    process = psutil.Process()
+    print(f"{process.memory_info().rss / 1000 / 1000:0.02f}MB")
+    env = gym.make("PushCube-v1", num_envs=1, obs_mode="none", reward_mode="sparse")
+    env.reset(seed=0)
+    i = 0
+    while True:
+        # env.unwrapped._scene.px.cuda_rigid_body_data # this is fine, no increase in mem
+        # env.unwrapped._scene.px.cuda_rigid_body_data.torch() # this increases memory
+        i += 1
+        env.step(None)
+        if i % 100 == 0:
+            gc.collect()
+            print(f"{i}, {process.memory_info().rss / 1000 / 1000:0.02f}MB")
+
+
+if __name__ == "__main__":
+    main()
